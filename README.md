5e68aad30db7e225c26b53302e547de64aa632f5
Azaz Moon <Sp18-bcs-cuiwah.edu.pk>
2021-09-03 11:55:18 +0500

    Issues Fixed/Slider Added/Website looks perfect now on 4k as well

---
+++ README.md
@@ -0,0 +1,12 @@
+# Southern-Interiors
+
+## All pages are done
+
++ ### "Individual project" page is linked with the "projects" link in footer.
++ ### Get a Quote is currently linked with contact page. 
+
+<br/>
+
+![Home](https://user-images.githubusercontent.com/64412852/131700595-dfa21142-40d2-4c41-8f3c-abe73ecb4b15.png)
+
+
---
+++ js/app.js
@@ -0,0 +1,66 @@
+// >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<
+// >>>>>>>>> TESTIMONIAL SLIDER <<<<<<<<<
+// >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<
+
+var $slickEl = $(".testimoninal-slider");
+
+$slickEl.slick({
+  slidesToShow: 1,
+  dots: false,
+  infinite: true,
+  prevArrow:
+    '<img class="prev-feedback" src="./images/svg/prev.svg" onerror="this.onerror=null;this.src="../images/svg/prev.svg" alt="Previous Feedback" />',
+  nextArrow:
+    '<img class="next-feedback" src="./images/svg/next.svg" alt="Next Feedback" />',
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+// >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<
+// >>>>>>>>>>> IMAGE GALLERY <<<<<<<<<<<<
+// >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
